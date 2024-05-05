f19e1b34b90b1a79ab063baf1e4d86ba202316f0
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-05 15:13:27 +0530

    Install firebase and react-firebase-hooks

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA0IhiXHG25T5A0DQSG5OKDmz5mttuqpCQ",
+    authDomain: "netflix-clone-with-react-8cee4.firebaseapp.com",
+    projectId: "netflix-clone-with-react-8cee4",
+    storageBucket: "netflix-clone-with-react-8cee4.appspot.com",
+    messagingSenderId: "762578990375",
+    appId: "1:762578990375:web:02cca11439155ad852a502"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
