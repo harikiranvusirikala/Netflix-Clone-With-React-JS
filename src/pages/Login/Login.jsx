f2aac55236e98b63d3c002ad89bf177b9148f244
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-05-06 11:04:32 +0530

    Implement Log In and Sign Up in Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { signup, login } from '../../firebase'
 
 const Login = () => {
 
   const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = async (event) => {
+    event.preventDefault(); // to prevent page reload on submit
+    if (signState === "Sign In") {
+      await login(email, password)
+    } else {
+      await signup(name, email, password)
+    }
+  }
 
   return (
     <div className='login'>
@@ -12,10 +25,11 @@ const Login = () => {
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState === "Sign Up" ? <input type="text" placeholder='Your name' /> : <></>}
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
-          <button>{signState}</button>
+          {signState === "Sign Up" ?
+            <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='Your name' /> : <></>}
+          <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder='Email' />
+          <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Password' />
+          <button onClick={user_auth}>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -27,7 +41,7 @@ const Login = () => {
         <div className="form-switch">
           {signState === "Sign In" ?
             <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p> :
-            <p>Already have account? <span onClick={() => { setSignState("Sign In")  }}>Sign In Now</span></p>
+            <p>Already have account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>
           }
         </div>
       </div>
