51902d8f0b9d13d7001c6d25bd778fc652f8c3f2
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-03-23 12:11:01 +0530

    Revert changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,18 @@ import { addDoc, collection, getDoc, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyA0IhiXHG25T5A0DQSG5OKDmz5mttuqpCQ",
+    authDomain: "netflix-clone-with-react-8cee4.firebaseapp.com",
+    projectId: "netflix-clone-with-react-8cee4",
+    storageBucket: "netflix-clone-with-react-8cee4.appspot.com",
+    messagingSenderId: "762578990375",
+    appId: "1:762578990375:web:02cca11439155ad852a502"
 };
 
 const app = initializeApp(firebaseConfig);
