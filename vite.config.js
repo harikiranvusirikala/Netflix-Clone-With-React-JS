cb4f83ca6150618ef8f64f924ed51b4e0eb7ee0f
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-03-23 11:52:44 +0530

    Fix deploy issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Netflix-Clone-With-React-JS/",
+  // base: "/Netflix-Clone-With-React-JS/",
   plugins: [react()],
   build: {
     outDir: 'Netflix-Clone-With-React-JS/dist', // Ensure the output directory is 'dist'
