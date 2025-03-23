1934121b93aac4b9267b0ac7703b2ca1ad26f81c
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-03-23 11:42:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Netflix-Clone-With-React-JS/",
+  // base: "/Netflix-Clone-With-React-JS/",
   plugins: [react()],
-  build: {
-    outDir: '/Netflix-Clone-With-React-JS/dist', // Ensure the output directory is 'dist'
-  },
+  // build: {
+  //   outDir: '/Netflix-Clone-With-React-JS/dist', // Ensure the output directory is 'dist'
+  // },
 })
