b84d68cb95b8773e34fe8d30d1e554f238e6cf4a
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-03-23 11:38:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Netflix-Clone-With-React-JS/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is 'dist'
+  },
 })
